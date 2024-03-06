f15647239d7102e4d36a44664a240f97924172af
rambaaral <152002088+rambaaral@users.noreply.github.com>
2024-03-06 06:35:33 +0000

    aa

---
+++ lec02/hello.py
@@ -0,0 +1,7 @@
+print("Hello, World!")
+name = "홍길동"
+print(name)
+print("{}님 안녕하세요!".format(name))
+
+department = "스마트팜학과"
+print("{}({})님 안녕하세요!".format(name, department))
---
+++ lec02/hw02.py
@@ -0,0 +1,20 @@
+
+cd1 = 30
+cd2 = 0
+print((cd1 - 32)*5/9)
+print((cd2 - 32)*5/9)
+
+weight = 60
+height = 1.70
+BMI = weight/(height*height)
+print(BMI)
+
+r = 4
+c = r*r*3.141592
+print(c)
+
+a = 5
+b = 3
+h = 4
+s = (a + b)/2*h
+print(s)
