9e6a3762c7065b9be42637c92116bada3df8b6df
rambaaral <152002088+rambaaral@users.noreply.github.com>
2024-03-07 12:45:54 +0900

    Update hw02.py

--- lec02/hw02.py
+++ lec02/hw02.py
@@ -1,18 +1,18 @@
 
 cd1 = 30
 cd2 = 0
-fd1 = (cd1 - 32)*5/9
-fd2 = (cd2 - 32)*5/9
+fd1 = round((cd1 - 32)*5/9, 2)
+fd2 = round((cd2 - 32)*5/9, 2)
 print("섭씨 {}도는 화씨 {}도 입니다.".format(cd1, fd1))
 print("섭씨 {}도는 화씨 {}도 입니다.".format(cd2, fd2))
 
 weight = 60
-height = 1.70
-BMI = weight/(height*height)
-print("몸무게가 {}kg, 키가 {}m인 사람의 BMI수치는 {}입니다.".format(weight, height, BMI))
+height = 170
+BMI = round(weight/(height/100)**2, 2)
+print("몸무게가 {}kg, 키가 {}cm인 사람의 BMI수치는 {}입니다.".format(weight, height, BMI))
 
 r = 4
-c = r*r*3.141592
+c = round(r*r*3.141592, 2)
 print("반지름이 {}인 원의 넓이는 {}입니다.".format(r, c))
 
 a = 5
