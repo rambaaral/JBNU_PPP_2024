b13b73dd49856ae12ab11ed75237068834a34c7d
rambaaral <152002088+rambaaral@users.noreply.github.com>
2024-03-13 15:49:10 +0900

    Add files via upload

--- hw/hw03/hw03_06.py
+++ hw/hw03/hw03_06.py
@@ -1,1 +1,9 @@
 #두 지점 사이 거리 구하기
+x1=int(input("첫 번째 점의 x좌표를 입력해주세요."))
+y1=int(input("첫 번째 점의 y좌표를 입력해주세요."))
+x2=int(input("두 번째 점의 x좌표를 입력해주세요."))
+y2=int(input("두 번째 점의 y좌표를 입력해주세요."))
+import math
+le=round(math.sqrt(((x1-x2)**2)+((y1-y2)**2)), 2)
+
+print("두 점 사이의 거리는 {}입니다.".format(le))
