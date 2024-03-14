c30a4ec0d172e7a13a4ddc7ad90c55119debb737
rambaaral <152002088+rambaaral@users.noreply.github.com>
2024-03-14 12:59:53 +0900

    Update hw03_02.py

--- hw/hw03/hw03_02.py
+++ hw/hw03/hw03_02.py
@@ -1,6 +1,7 @@
 #실습 2, 3 완성
 w=float(input("몸무게를 입력해주세요.(kg)"))
 h=float(input("키를 입력해주세요.(cm)"))
-bmi=round(w/(h/100)**2, 2)
+import math
+bmi=round(w/math.pow(h/100, 2), 2)
 
 print("몸무게가 {}kg, 키가 {}cm인 사람의 BMI수치는 {}입니다.".format(w, h, bmi))
