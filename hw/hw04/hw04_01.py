75bbc2db246e19dec99b5fe6f53cc76681deb42b
rambaaral <rambaral@jbnu.ac.kr>
2024-03-18 20:43:29 +0900

    hw04 final

--- hw/hw04/hw04_01.py
+++ hw/hw04/hw04_01.py
@@ -1,1 +1,18 @@
 #BMI 수치 비만 정도 표시
+w=float(input("몸무게를 입력해주세요.(kg)"))
+h=float(input("키를 입력해주세요.(cm)"))
+import math
+bmi=round(w/math.pow(h/100, 2), 2)
+
+print("몸무게가 {}kg, 키가 {}cm인 사람의 BMI수치는 {}입니다.".format(w, h, bmi))
+
+if bmi < 23:
+    print("비만이 아닙니다.")
+elif bmi < 25:
+    print("비만 전단계 입니다.")
+elif bmi < 30:
+    print("1단계 비만입니다.")
+elif bmi < 35:
+    print("2단계 비만입니다.")
+else:
+    print("3단계 비만입니다.")
--- hw/hw04/hw04_02.py
+++ hw/hw04/hw04_02.py
@@ -1,1 +1,16 @@
 #x, y 입력 받아서 어느 사분면인지 출력
+x1=int(input("점의 x좌표를 입력해주세요."))
+y1=int(input("점의 y좌표를 입력해주세요."))
+
+if x1 == 0 or y1 == 0:
+    print("점이 어느 사분면에도 속하지 않습니다.")
+elif x1 > 0 and y1 > 0:
+    print("점이 1사분면 위에 있습니다.")
+elif x1 < 0 and y1 > 0:
+    print("점이 2사분면 위에 있습니다.")
+elif x1 < 0 and y1 < 0:
+    print("점이 3사분면 위에 있습니다.")
+elif x1 > 0 and y1 <0:
+    print("점이 4사분면 위에 있습니다.")
+else:
+    print("올바르지 않은 접근입니다.")
