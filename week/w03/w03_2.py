f82a1c1709cb35d6fd75f96738b3e9c8e92c46d3
rambaaral <rambaral@jbnu.ac.kr>
2024-03-20 14:35:36 +0900

    asdf

---
+++ week/w03/w03_2.py
@@ -0,0 +1,15 @@
+#포맷팅
+aaaaa = 3.14159362
+print('asdf{:.3f}'.format(aaaaa))
+print(f'asdf{aaaaa:.3f}')
+
+#반복문 for, while
+#for는 주어진 리스트를 모두 소진할 동안 반복
+#while은 주어진 조건이 참일 때 게속 실행
+#반복문을 특정 조건에 멈출 경우 break나 continue를 사용한다.
+for i in range(10):
+    print(i+1)
+
+for i in range(10):
+    print(i+1)
+
