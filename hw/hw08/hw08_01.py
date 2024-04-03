c7124e379f2acdbd36279919292a2e1f997d0ec0
rambaaral <rambaral@jbnu.ac.kr>
2024-04-03 14:18:18 +0900

    mm

--- hw/hw08/hw08_01.py
+++ hw/hw08/hw08_01.py
@@ -4,26 +4,27 @@ def text2list(a):
     result = []
     for token in tokens:
         result.append(int(token))
-    return sorted(result)
+    return result
 
 def average(a):
     result = sum(a)/len(a)
     return result
 
 def median(a):
-    l = int(len(a)/2)
-    result = a[(l)]
+    length = len(a)//2
+    result = a[length]
     return result
 
 def main():
     input_text = "5 10 3 4 7"
     nums = text2list(input_text)
+    sornums = sorted(nums)
     print("주어진 리스트는", nums)
-    print("평균값은 {:.1f}".format(average(nums)))
-    print("중앙값은 {}".format(median(nums)))
+    print("평균값은 {:.1f}".format(average(sornums)))
+    print("중앙값은 {}".format(median(sornums)))
     #단 갯수가 짝수일 때는 중앙에 위치한 두 값 중 큰 값을 채택
-    print(f"최솟값은 {nums[0]}")
-    print(f"최댓값은 {nums[len(nums)-1]}")
+    print(f"최솟값은 {sornums[0]}")
+    print(f"최댓값은 {sornums[-1]}")
 
 if __name__ == "__main__":
     main()
